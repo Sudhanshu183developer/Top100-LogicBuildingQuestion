36155e30b42cf4f964c5bbb2122804a8c59d10ce
sudhanshu sachan <sudhanshusachan9@gmail.com>
2026-02-17 10:24:52 +0530

    1st-Program

---
+++ CDigit.java
@@ -0,0 +1,11 @@
+public class CDigit {
+       public static void main(String[] args){
+              int n = 123;
+              int count = 0;
+              while(n > 0){
+                   count = count+1;
+                   n = n/10;
+              }
+              System.out.println(count);
+       }
+}
