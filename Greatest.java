3839633fb19327ae19bea543302a94e2844b0149
sudhanshu sachan <sudhanshusachan9@gmail.com>
2026-02-18 21:54:03 +0530

    Find the greatest of three numbers

---
+++ Greatest.java
@@ -0,0 +1,16 @@
+public class Greatest {
+       public static void main(String[] args){
+              int a = 100;
+              int b = 110;
+              int c = 140;
+              if (a >= b && a >= c){
+                  System.out.println("A is greatest Number ");
+              }
+              else if ( b >=a && b >= c){
+                    System.out.println("B is Greatest Number");
+              }
+              else {
+                System.out.println("C is Greatest Number :");
+              }
+       }
+}
