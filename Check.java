8b2800f8fed8f55e47a3e2f9bd7f5cddbdcb06d9
sudhanshu sachan <sudhanshusachan9@gmail.com>
2026-02-18 21:47:06 +0530

    . Check if a number is positive, negative, or zero

---
+++ Check.java
@@ -0,0 +1,14 @@
+public class Check {
+       public static void main(String[] args){
+              int x = 0;
+              if(x == 0){
+                 System.out.println("Zero Number :");
+              }
+              else if (x <= 0){
+                  System.out.println(" Negative Number :");
+              }
+              else {
+                System.out.println("Positive Number :");
+              }
+       }
+}
